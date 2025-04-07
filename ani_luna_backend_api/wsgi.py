8bf92431faffd02caea8b9fea5ac8bd8e57a8c6f
Drew <hilarioandrew12@gmail.com>
2025-04-07 19:55:57 +0800

    refactor wsgi.py for consistency and readability; standardize string quotes

--- ani_luna_backend_api/wsgi.py
+++ ani_luna_backend_api/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ani_luna_backend_api.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ani_luna_backend_api.settings")
 
 application = get_wsgi_application()
+
+
+app = application  # For Gunicorn compatibility
